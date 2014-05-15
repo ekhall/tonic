0062f42cf1f54dc293d5e7583c96b9f408d52b7e
E. Kevin Hall <hall@absinthe.org>
2014-05-15 15:38:01 -0400

    Cleanup of test code

--- test/tonic/core_test.clj
+++ test/tonic/core_test.clj
@@ -4,12 +4,11 @@
             [datomic.api :only (q db) :as d]
             ))
 
-(defn erase-dev-db []
-  (let [uri "datomic:dev://localhost:4334/test-db"]
-    (d/delete-database uri)))
+(def uri-url "datomic:dev://localhost:4334/test-db")
 
-(defn create-empty-dev-db []
-  (let [uri "datomic:dev://localhost:4334/test-db"]
+;; Opens db
+(defn open-dev-db []
+  (let [uri uri-url]
     ;;(d/delete-database uri)
     (d/create-database uri)
     (let [conn (d/connect uri)
@@ -17,23 +16,26 @@
       @(d/transact conn schema)
       conn)))
 
-(create-empty-dev-db)
+(defn clean-db []
+  (d/delete-database uri-url))
 
 (defn show-schema []
-  (with-redefs [conn (create-empty-dev-db)]
+  (with-redefs [conn (open-dev-db)]
     (do
       (d/q '[:find ?ident
        :where [_ :db/ident ?ident]]
-     (d/db conn)))))
+           (d/db conn)))))
 
-(defn test-add-person [lastName]
-  (with-redefs [conn (create-empty-dev-db)]
+(defn show-persons []
+  (with-redefs [conn (open-dev-db)]
     (do
-      (add-person (str lastName "-0"))
-      (add-person (str lastName "-1"))
       (d/q '[:find ?lastName
              :where [?e :person/lastName ?lastName]]
            (d/db conn)))))
 
-(test-add-person "Bubba")
-(test-add-person "Rich")
+(defn test-add-person [lastName]
+  (with-redefs [conn (open-dev-db)]
+    (do
+      (add-person (str lastName "-0"))
+      (add-person (str lastName "-1"))
+      (d/db conn))))
